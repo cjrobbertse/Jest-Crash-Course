276e274bda0c4c6775d131f8b2d7d98445cbd88b
Christopher Robbertse <christopher.robbertse@vodafone.com>
2019-10-01 12:04:42 +0100

    tests

---
+++ test/functions.test.js
@@ -0,0 +1,11 @@
+/* eslint-env jest */
+
+const functions = require('../src/functions')
+
+test('Adds 2 + 2 to equal 4', () => {
+  expect(functions.add(2, 2)).toBe(4)
+})
+
+test('Adds 2 + 2 to NOT equal 5', () => {
+  expect(functions.add(2, 2)).not.toBe(4)
+})
